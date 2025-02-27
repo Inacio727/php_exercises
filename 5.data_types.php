4c86caf4d9a0a2ad60c21adf30bcd6c9e3d55390
John Doe <johndoe@example.com>
2025-02-27 16:06:28 -0300

    6strings

---
+++ 5.data_types.php
@@ -0,0 +1,96 @@
+<?php
+
+//getting the data type
+
+// $x = 5;
+// var_dump($x);
+// echo "<br>";
+
+
+
+
+// $x = "Hello world!";
+// $y = 'Hello world!';
+
+// var_dump($x);
+// echo "<br>";
+// var_dump($y);
+
+
+
+
+// $x = 5985;
+// var_dump($x);
+// echo "<br>";
+
+
+
+
+// $x = 10.365;
+// var_dump($x);
+// echo "<br>";
+
+
+
+
+// $x = false;
+// var_dump($x);
+// echo "<br>";
+
+
+
+// $cars = array("Volvo", 1.90, false, 25);
+// var_dump($cars);
+// echo "<br>";
+
+
+
+// class Car {
+//     public $color;
+//     public $model;
+//     public function __construct($color, $model) {
+//         $this -> color = $color;
+//         $this -> model = $model;
+//     }
+
+//     public function message() {
+//         return "My car is a " . $this -> color . " " . $this -> model . "!";
+//     }
+// }
+
+
+// $myCar = new Car("black", "Volvo");
+// var_dump($myCar);
+// echo "<br>";
+
+
+
+// $x = "Hello world!";
+// var_dump($x);
+// echo "<br>";
+
+// change data type
+
+// $x = 5;
+// var_dump($x);
+
+// echo "<br>";
+
+// $x = "Carlos";
+// var_dump($x);
+
+
+$x = 5;
+var_dump($x);
+echo "<br>";
+
+$x = (string) $x;
+var_dump($x);
+
+
+
+
+
+
+
+?>
---
+++ 6.strings.php
@@ -0,0 +1,20 @@
+<?php
+
+// $x = "John";
+// echo "Hello $x!";
+// echo "<br>";
+// echo 'Hello $x!';
+// echo "<br>";
+
+
+// echo strlen("Hello world!"); // outputs 12
+
+// echo str_word_count("Hello world!"); // outputs 2
+
+echo strpos("Hello world!", "llo world");
+
+
+
+
+
+?>
